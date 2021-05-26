5f728eef98e9bb44d622eea877a2171fdf3a1f25
Isaiah Guardado <37196217+crazay416@users.noreply.github.com>
2021-05-26 15:19:22 -0700

    Update Recursion.java

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -3,14 +3,25 @@ package algorithms;
 public class Recursion {
 	
 	public static void main(String[] args) {
-		fun(5);
+		System.out.println("Prints number before recursion");
+		fun1(3);
+		System.out.println("\nPrints number after recursion recursion");
+		fun2(3);
 	}
 	
 	
-	static void fun(int n) {
+	static void fun1(int n) {
 		if (n > 0) {
 			System.out.println(n);
-			fun(n - 1);
+			fun1(n - 1);
+		}
+	}
+	
+	
+	static void fun2(int n) {
+		if (n > 0) {
+			fun2(n - 1);
+			System.out.println(n);
 		}
 	}
 
