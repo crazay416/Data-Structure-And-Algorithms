f96b5a6dfd6cb639391cf163ff491aa7713b3be3
Isaiah Guardado <37196217+crazay416@users.noreply.github.com>
2021-05-25 17:20:25 -0700

    Update Recursion.java

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,27 +1,16 @@
-package recursion;
+package algorithms;
 
 public class Recursion {
 	
 	public static void main(String[] args) {
-		int num = 3;
-		System.out.print("This is func1: ");
-		func1(num);
-		
-		System.out.print("\nThis is func2: ");
-		func2(num);
+		fun(5);
 	}
 	
-	public static void func1(int x) {
-		if(x > 0) {
-			System.out.print(x + " ");
-			func1(x - 1);
-		}
-	}
 	
-	public static void func2(int x) {
-		if(x > 0) {
-			func2(x - 1);
-			System.out.print(x + " ");
+	static void fun(int n) {
+		if (n > 0) {
+			System.out.println(n);
+			fun(n - 1);
 		}
 	}
 
